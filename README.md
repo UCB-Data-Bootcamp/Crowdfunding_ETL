739583219dbc7fe2c461f6fe08abc2908f976957
surfaki <143300729+surfaki@users.noreply.github.com>
2023-11-30 21:17:05 -0800

    final submission

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Crowdfunding_ETL
 UCB Bootcamp project#2
 
+# Git repo terminal commands
 git status
 git add .
 git commit -m "final submission"
 git push
+
+# Querying postgress from Jupyter
+Used the following command to create the engine in order to run the query:
+engine = create_engine('postgresql://postgres:*****@localhost:5432/crowdfunding_db')
+
---
+++ final/Schema/crowdfunding_db_schema.sql
@@ -0,0 +1,47 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL PRIMARY KEY,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL PRIMARY KEY,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR   NOT NULL PRIMARY KEY,
+    "category" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR   NOT NULL PRIMARY KEY,
+    "subcategory" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
